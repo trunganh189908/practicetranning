ec18f0018b621137b81c99541ecf9efcb3cf045b
ĐỖ ANH TRUNG <trung@Tr-anh.local>
2022-07-18 16:00:03 +0700

    Synandvar

---
+++ syntaxandvariables/syntax.js
@@ -0,0 +1,25 @@
+const ro = parseInt(prompt('enter price ronaldo='));
+
+// white space
+
+const mes = parseInt(prompt('enter price messi=')); // ; is the end of statements
+
+
+// white space 
+
+const sum = ro + mes;
+
+console.log(`Total psg buy ronaldo with ${ro} and messi ${mes} is ${sum} Euro `);
+
+// {code in here} => block
+
+if (i => 6) {
+    console.log('hoc sinh kha');
+}
+else {
+    console.log('hoc sinh trung binh')
+}
+
+// behind // is comment
+
+/* is block comment */
---
+++ syntaxandvariables/variables.js
@@ -0,0 +1,29 @@
+//decale variable
+
+var variables;
+
+let variables2;
+
+// Initialize a variable
+let message;
+message = "Hello";
+
+let variableName = value;
+
+let notification = "Hello",
+    counter = 100;
+
+
+// Undefined vs. undeclared variables
+
+let testvaria;
+console.log(testvaria); // undefined
+
+console.log(counter); //counter is not defined
+
+
+// Contrast
+
+const workday = 5;
+g
+
